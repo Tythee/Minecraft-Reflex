3edf1044051c484e4334d9def855e7432856ec46
OrzMiku <miku@ecy.pink>
2025-03-10 14:04:08 +0800

    1.20.1 MultiLoader

---
+++ forge/src/main/java/io/tythee/forge/ReflexClientForge.java
@@ -0,0 +1,18 @@
+package io.tythee.forge;
+
+import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import io.tythee.ReflexClient;
+import io.tythee.config.ConfigScreen;
+
+@Mod(ReflexClient.MOD_ID)
+public final class ReflexClientForge {
+    @SuppressWarnings("removal")
+    public ReflexClientForge() {
+        ReflexClient.init();
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> {
+            return ConfigScreen.create(parent);
+        }));
+    }
+}
