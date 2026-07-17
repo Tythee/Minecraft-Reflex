866a97dcbdc1bc1f898bce37af6222ee2d59b691
jason54 <jason54700.jg@gmail.com>
2026-07-17 21:35:08 +0200

    Migrate to Stonecutter multi-version build (1.21.10 / 1.21.11 / 26.1.2 / 26.2, Fabric + NeoForge)

---
+++ src/main/java/io/tythee/neoforge/ReflexClientNeoforge.java
@@ -0,0 +1,24 @@
+package io.tythee.neoforge;
+
+//? if neoforge {
+/*import net.minecraft.client.gui.screens.Screen;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import io.tythee.ReflexClient;
+import io.tythee.config.ConfigScreen;
+
+@Mod(value = ReflexClient.MOD_ID, dist = Dist.CLIENT)
+public final class ReflexClientNeoforge {
+    public ReflexClientNeoforge(ModContainer container) {
+        ReflexClient.init();
+        container.registerExtensionPoint(IConfigScreenFactory.class, new IConfigScreenFactory() {
+            @Override
+            public Screen createScreen(ModContainer container, Screen modListScreen) {
+                return ConfigScreen.create(modListScreen);
+            }
+        });
+    }
+}
+*///?}
